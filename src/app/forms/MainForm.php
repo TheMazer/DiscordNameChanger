33f6c2faf6e1b3c1968e2dd34ed4b5d31c07628c
MazepaImpostor <ryzhenkovzhenya03@gmail.com>
2023-08-25 00:23:52 +0300

    Full Working Scripts - Stable

--- src/app/forms/MainForm.php
+++ src/app/forms/MainForm.php
@@ -21,6 +21,8 @@ class MainForm extends AbstractForm
         $VBox->padding = 16;
         $this->container->content->add($VBox);
         
+        $this->checkChangingButtons();
+        
         //var_dump($elements[1]->children[1]->text);
     }
 
@@ -68,11 +70,58 @@ class MainForm extends AbstractForm
      */
     function doStartBtnAction(UXEvent $e = null)
     {    
+        global $working, $names, $nameIndex, $serverIDs, $auth, $enableTime, $updateSpeed;
+        
         // Parse elements
         $elements = $this->Box->children;
+        $names = [];
+        $nameIndex = 0;
+        $serverIDs = explode(',', str_replace(' ', '', $this->serverIdEdit->text));
+        $auth = $this->authEdit->text;
+        $enableTime = null;
         
         foreach ($elements as $elem) {
-            var_dump($elem->children[0]->text);
+            $names[] = $elem->children[0]->text;
+        }
+        
+        Logger::info('Nicknames: '. implode(', ', $names));
+        
+        $updateSpeed = intval($this->intervalField->text) * 1000;
+        $this->request->interval = $updateSpeed;
+        
+        if (!$working) {
+            $working = true;
+            $this->doRequestAction();
+            $this->request->enabled = true;
+            $e->sender->text = 'Стоп';
+            $e->sender->classesString = 'button danger';
+            $icon = new UXImage('res://.data/img/pause.png');
+            $e->sender->graphic = new UXImageView($icon);
+        } else {
+            $working = false;
+            $this->request->enabled = false;
+            $e->sender->text = 'Старт';
+            $e->sender->classesString = 'button success';
+            $icon = new UXImage('res://.data/img/play.png');
+            $e->sender->graphic = new UXImageView($icon);
+        }
+        
+        $this->panel->enabled = !$working;
+        $this->addNickBtn->enabled = !$working;
+        $this->importBtn->enabled = !$working;
+        $this->exportBtn->enabled = !$working;
+        $this->removeAllBtn->enabled = !$working;
+        $this->decrementBtn->enabled = !$working;
+        $this->intervalLabel->enabled = !$working;
+        $this->intervalField->enabled = !$working;
+        $this->incrementBtn->enabled = !$working;
+        $this->idLabel->enabled = !$working;
+        $this->serverIdEdit->enabled = !$working;
+        $this->authLabel->enabled = !$working;
+        $this->authEdit->enabled = !$working;
+        
+        if (!$working) {
+            $this->checkChangingButtons();
         }
     }
 
@@ -84,7 +133,58 @@ class MainForm extends AbstractForm
         $this->Box->children->clear();
     }
 
+    function checkChangingButtons() {
+        if (intval($this->intervalField->text) > 1) {
+            $this->decrementBtn->enabled = true;
+        } else {
+            $this->decrementBtn->enabled = false;
+        }
+        if (intval($this->intervalField->text) < 999999) {
+            $this->incrementBtn->enabled = true;
+        } else {
+            $this->incrementBtn->enabled = false;
+        }
+    }
+    
+    function showCD($text) {
+        if ($this->cdLabel->opacity <= 0) {
+            $this->cdLabel->position = [16, 112];
+            $this->cdLabel->text = $text;
+            $this->cdLabel->opacity = 1;
+            waitAsync(3000, function () {
+                Animation::fadeOut($this->cdLabel, 1000);
+            });
+            waitAsync(4000, function () {
+                $this->cdLabel->position = [672, 112];
+            });
+        }
+    }
 
+    /**
+     * @event decrementBtn.action 
+     */
+    function doDecrementBtnAction(UXEvent $e = null)
+    {    
+        $this->intervalField->text = intval($this->intervalField->text) - 1;
+        $this->checkChangingButtons();
+    }
+
+    /**
+     * @event incrementBtn.action 
+     */
+    function doIncrementBtnAction(UXEvent $e = null)
+    {    
+        $this->intervalField->text = intval($this->intervalField->text) + 1;
+        $this->checkChangingButtons();
+    }
+
+    /**
+     * @event intervalField.keyUp 
+     */
+    function doIntervalFieldKeyUp(UXKeyEvent $e = null)
+    {    
+        $this->checkChangingButtons();
+    }
 
 
 }
--- src/app/modules/MainModule.php
+++ src/app/modules/MainModule.php
@@ -7,4 +7,126 @@ use std, gui, framework, app;
 class MainModule extends AbstractModule
 {
 
+    /**
+     * @event request.action 
+     */
+    function doRequestAction(ScriptEvent $e = null)
+    {    
+        /*$data = '{"global_name":"!Tractor 228"}';
+        $url = 'https://discord.com/api/v9/users/@me';
+        $headers = [
+            'Content-Type: application/json',
+            'Authorization: NTcyMDUxMjg5MDg5OTAwNTQ1.GWF5Fa.ja_3lr3xDhl_esg4erSPpy4ER3XulEBzqKuk4k'
+        ];
+        $curl = curl_init($url);
+        
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        
+        $response = curl_exec($curl);
+        var_dump($response);
+        curl_close($curl);*/
+        
+        /*$ch = curl_init('https://discord.com/api/v9/users/@me');
+        
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
+        curl_setopt($ch, CURLOPT_PATCH, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, '{"global_name":"!Tractor 228"}');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-type: application/json', 'Authorization: NTcyMDUxMjg5MDg5OTAwNTQ1.GWF5Fa.ja_3lr3xDhl_esg4erSPpy4ER3XulEBzqKuk4k']);
+        
+        var_dump(curl_exec($ch));*/
+        
+        /*$httpClient = new HttpClient();
+        $httpClient->headers = [
+            'Content-Type: application/json',
+            'Authorization: NTcyMDUxMjg5MDg5OTAwNTQ1.GWF5Fa.ja_3lr3xDhl_esg4erSPpy4ER3XulEBzqKuk4k'
+        ];
+        var_dump( $httpClient->patch('https://discord.com/api/v9/users/@me', '{"global_name":"!Tractor 228"}') );
+        
+        $conn = new URLConnection('https://discord.com/api/v9/users/@me');
+        $conn->setRequestProperty("X-HTTP-Method-Override", "PATCH");
+        $conn->setRequestProperty();
+        $conn->requestMethod = 'POST';*/
+        $reqThread = new Thread(function () use ($e) {
+            var_dump('____________ New Changing ____________');
+        
+            global $names, $nameIndex, $serverIDs, $auth, $enableTime, $updateSpeed;
+            //Logger::warn('Still sleeping... | '. time(). ' / '. $enableTime);
+
+            if (!$enableTime) {
+            
+                if ( substr($names[$nameIndex], 0, strlen("/sleep")) === "/sleep" ) {
+                    $sleepDur = intval(substr($names[$nameIndex], strlen("/sleep"), strlen($names[$nameIndex])-1)) - 1;
+                    $enableTime = time() + $sleepDur;
+                    $e->sender->interval = 1000;
+                    Logger::info('Sleeping '. $sleepDur. ' seconds');
+                } else {
+                    
+                    if ($serverIDs) {
+                        foreach ($serverIDs as $serverID) {
+                            $serverUrl = '&serverId='. $serverID;
+                            $url =
+                                'http://ryzhenkovz.temp.swtest.ru/dsChangeNick.php?'.
+                                'name='. urlencode($names[$nameIndex]). '&'.
+                                'auth='. $auth.
+                                $serverUrl;
+                            var_dump($url);
+                            
+                            $result = file_get_contents($url);
+                            var_dump('Response: '. $result);
+                            
+                            $responseData = json_decode($result, true);
+                            if ($responseData['message'] == 'You are being rate limited.') {
+                                uiLater(function () use ($responseData) {
+                                    $this->form('MainForm')->showCD('Discord отправил вас в Cooldown'. "\n". 'Осталось: '. strval(intval($responseData['retry_after'])).
+                                    ' сек.'. "\n". 'Осторожнее!');
+                                });
+                            }
+                        }
+                    } else {
+                        $url =
+                            'http://ryzhenkovz.temp.swtest.ru/dsChangeNick.php?'.
+                            'name='. urlencode($names[$nameIndex]). '&'.
+                            'auth='. $auth;
+                        var_dump($url);
+                        
+                        $result = file_get_contents($url);
+                        var_dump('Response: '. $result);
+                        
+                        $responseData = json_decode($result, true);
+                        if ($responseData['message'] == 'You are being rate limited.') {
+                            uiLater(function () use ($responseData) {
+                                $this->form('MainForm')->showCD('Discord отправил вас в Cooldown'. "\n". 'Осталось: '. strval(intval($responseData['retry_after'])).
+                                ' сек.'. "\n". 'Осторожнее!');
+                            });
+                        }
+                        // $result = '{"message":"You are being rate limited.","retry_after":177.213,"global":false}';
+                        // alert(json_decode($result, true)['message']);
+                    }
+                
+                    $nameIndex++;
+                
+                    if ($nameIndex > count($names) - 1) {
+                        $nameIndex = 0;
+                    }
+
+                }
+            } else {
+                if (time() >= $enableTime) {
+                    $e->sender->interval = $updateSpeed;
+                    $enableTime = null;
+                    $nameIndex++;
+                    
+                    if ($nameIndex > count($names) - 1) {
+                        $nameIndex = 0;
+                    }
+                }
+            }
+        });
+        
+        $reqThread->start();
+    }
+
 }
